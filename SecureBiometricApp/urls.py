5078f3028bc1054c5a8568235a5bbabb057a9aa2
toxicy57 <cb232923@gmail.com>
2026-05-09 22:32:36 +0530

    Fix root URL

--- SecureBiometricApp/urls.py
+++ SecureBiometricApp/urls.py
@@ -1,18 +1,29 @@
 from django.urls import path
-
 from . import views
 
-urlpatterns = [path("index.html", views.index, name="index"),
-			path("Login.html", views.Login, name="Login"),
-			path("Signup.html", views.Signup, name="Signup"),
-			path("SignupAction", views.SignupAction, name="SignupAction"),	    	
-			path("UserLoginAction", views.UserLoginAction, name="UserLoginAction"),
-			path("CaptureFaceAction", views.CaptureFaceAction, name="CaptureFaceAction"),
-			path("ValidateFaceAction", views.ValidateFaceAction, name="ValidateFaceAction"),
-			path("WebCam", views.WebCam, name="WebCam"),
-			path("ValidateFace.html", views.ValidateFace, name="ValidateFace"),
-			path("Upload.html", views.Upload, name="Upload"),
-			path("UploadAction", views.UploadAction, name="UploadAction"),	
-			path("Download", views.Download, name="Download"),
-			path("DownloadFileAction", views.DownloadFileAction, name="DownloadFileAction"),
+urlpatterns = [
+
+    # HOME PAGE
+    path("", views.index, name="index"),
+    path("index.html", views.index, name="index"),
+
+    # LOGIN
+    path("Login.html", views.Login, name="Login"),
+    path("UserLoginAction", views.UserLoginAction, name="UserLoginAction"),
+
+    # SIGNUP
+    path("Signup.html", views.Signup, name="Signup"),
+    path("SignupAction", views.SignupAction, name="SignupAction"),
+
+    # FACE
+    path("CaptureFaceAction", views.CaptureFaceAction, name="CaptureFaceAction"),
+    path("ValidateFace.html", views.ValidateFace, name="ValidateFace"),
+    path("ValidateFaceAction", views.ValidateFaceAction, name="ValidateFaceAction"),
+    path("WebCam", views.WebCam, name="WebCam"),
+
+    # FILES
+    path("Upload.html", views.Upload, name="Upload"),
+    path("UploadAction", views.UploadAction, name="UploadAction"),
+    path("Download", views.Download, name="Download"),
+    path("DownloadFileAction", views.DownloadFileAction, name="DownloadFileAction"),
 ]
